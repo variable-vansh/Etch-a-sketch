81566eee595bdc8f071e69d67412b3eccf3c8cc3
Vansh Mishra <144242983+variable-vansh@users.noreply.github.com>
2024-07-01 13:32:36 +0530

    added stylesheet, resize button works now

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const container = document.querySelector("#container");
 container.style.backgroundColor = "red";
 
-//set the contents of whole page to be in the center horizontally
-const fullBody = document.querySelector('body');
-fullBody.style.display = "flex"
-fullBody.style.flexDirection = "column"
-fullBody.style.alignItems = "center"
-
-//initialize number of sides
-//calculate, total boxes
-//calculate width of each box
-let nSide = 10;
-let totalBoxes = nSide * nSide;
-let smallBoxWidth = 600 / nSide;
-
 //set size and flex properties to big box
 container.style.width = "600px"
 container.style.height = "600px"
 container.style.display = "flex";
 container.style.flexWrap = "wrap"
 
+//initialize number of sides
+let nSide = 16;
+let totalBoxes;
+let smallBoxWidth;
+
+basicCalculation();
+createMatrix(totalBoxes)
+
+//calculate, total boxes
+//calculate width of each box
+function basicCalculation() {
+    totalBoxes = nSide * nSide;
+    smallBoxWidth = 600 / nSide;
+    return smallBoxWidth;
+}
+
 //creates one small box
 function createOneSmallBox() {
+    basicCalculation();
     let innerBox = document.createElement("div");
     innerBox.classList.add("smallBox");
 
@@ -38,12 +42,27 @@ function createOneSmallBox() {
     container.append(innerBox)
 }
 
+//creates required number of small boxes
+//counts
+function createMatrix(totalBoxes) {
 
-let c = 0;
-for (let x = 1; x <= totalBoxes; x++) {
-    c++;
-    createOneSmallBox()
+    container.innerHTML = "";
+    for (let x = 1; x <= totalBoxes; x++) {
+        createOneSmallBox()
+    }
+
+    //gives out some stuff in the console
+    console.log(`The container is divided into ${nSide} x ${nSide}} boxes`)
+    console.log(`The small box has a side length of ${smallBoxWidth}`)
 }
 
-console.log(`The container is divided into ${nSide} x ${nSide} = ${c} boxes`)
-console.log(`The small box has a side length of ${smallBoxWidth}`)
+const resizeButton = document.querySelector("#resize");
+resizeButton.addEventListener("click", resize)
+
+
+//event handler for resize button click
+function resize() {
+    nSide = prompt("you clicked the resize button")
+    totalBoxes = nSide * nSide;
+    createMatrix(totalBoxes);
+}
